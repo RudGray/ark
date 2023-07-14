f32f2c25910851786f84956ef2e7858fdb9a7de7
RudGray <rud.gray@gmail.com>
2023-07-14 21:00:45 +0200

    initialisation de parse comme database

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+var Parse = require('parse/node');
+
+// la deuxième clé est une clé de compte utilisateur back4app, spécialement créée pour ce projet
+Parse.initialize("4f58d833-4dc8-4ca2-bd19-3dc4dc4c75b5", "84NEosmgaXPVvhW8EaWuSMbd8yPmosBkbnauIQLn");
+Parse.serverURL = 'https://YOUR_PARSE_SERVER_URL/parse'
+
 
 // Répertoire pour les fichiers statiques
 app.use(express.static(path.join(__dirname, '.')));
